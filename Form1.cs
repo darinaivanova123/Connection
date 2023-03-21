b8971e78d59338f209eb2229512a543a2bf7f482
Your Name <you@example.com>
2023-03-21 10:46:18 +0200

    SQL

---
+++ Form1.cs
@@ -0,0 +1,24 @@
+using MySqlConnector;
+
+namespace ConnectMySQL11d
+{
+    public partial class Form1 : Form
+    {
+        string connstr = "server=" +
+            "port=3306;" +
+            "user=REMOTE;" +
+            "password=Aa123456@;" +
+            "datebase=monions";
+        MySqlConnection 
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void checkedListBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+                                                                                                         
+
+        }
+    }
+}                                             
